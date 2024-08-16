de0851d81f84a406f4d66e96a1ee03ba3883921d
Derek Riter <derekriter08@gmail.com>
2024-08-16 12:12:46 -0400

    setup TIGR and made hello world window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include "tigr.h"
+
+//16:9 ratio
+#define BITMAP_W 48
+#define BITMAP_H 27
+#define SCREEN_W (BITMAP_W * 16)
+#define SCREEN_H (BITMAP_H * 16)
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Tigr* screen = tigrWindow(SCREEN_W, SCREEN_H, "CGoL", TIGR_FIXED);
+
+    while(!tigrClosed(screen)) {
+        tigrClear(screen, tigrRGB(0, 0, 0));
+
+        tigrPrint(screen, tfont, 0, 0, tigrRGB(255, 255, 255), "Hello, world!");
+
+        tigrUpdate(screen);
+    }
+    tigrFree(screen);
 
     return 0;
 }
